c5b582bbdffd4159e58dbd347577075b17b42449
negro-vinilo <52874360+negro-vinilo@users.noreply.github.com>
2024-01-23 16:15:04 +1100

    [vue] CoachFilter component updated by removing code duplication, changed data object properties and updated scoped style from css to scss; updated CoachesList component data structure to reflect CoachFilter modifications

--- src/components/coaches/CoachFilter.vue
+++ src/components/coaches/CoachFilter.vue
@@ -1,67 +1,71 @@
 <template>
-  <base-card>
-    <h2>Find Your Coach</h2>
-    <span class="filter-option">
-      <input type="checkbox" id="frontend" checked @change="setFilter" />
-      <label for="frontend">Frontend</label>
-    </span>
-    <span class="filter-option">
-      <input type="checkbox" id="backend" checked @change="setFilter" />
-      <label for="backend">Backend</label>
-    </span>
-    <span class="filter-option">
-      <input type="checkbox" id="career" checked @change="setFilter" />
-      <label for="career">Career</label>
-    </span>
-  </base-card>
+	<base-card>
+		<h2>Find Your Coach</h2>
+		<span
+			:class="['filter-option', { active: value.isActive }]"
+			v-for="(value, key) in filters"
+			:key="key"
+		>
+			<input type="checkbox" :id="key" checked @change="setFilter" />
+			<label :for="key" class="sentenceCase">{{ key }}</label>
+		</span>
+	</base-card>
 </template>
 
 <script>
 export default {
-  emits: ['change-filter'],
-  data() {
-    return {
-      filters: {
-        frontend: true,
-        backend: true,
-        career: true
-      }
-    };
-  },
-  methods: {
-    setFilter(event) {
-      const inputId = event.target.id;
-      const isActive = event.target.checked;
-      const updatedFilters = {
-        ...this.filters,
-        [inputId]: isActive
-      };
-      this.filters = updatedFilters;
-      this.$emit('change-filter', updatedFilters);
-    }
-  }
+	emits: ['change-filter'],
+	data() {
+		return {
+			filters: {
+				frontend: {
+					isActive: true,
+				},
+				backend: {
+					isActive: true,
+				},
+				career: {
+					isActive: true,
+				},
+			},
+		}
+	},
+	methods: {
+		setFilter(event) {
+			const inputId = event.target.id
+			const isChecked = event.target.checked
+			const updatedFilters = {
+				...this.filters,
+				[inputId]: {
+					isActive: isChecked,
+				},
+			}
+			this.filters = updatedFilters
+			this.$emit('change-filter', updatedFilters)
+		},
+	},
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 h2 {
-  margin: 0.5rem 0;
+	margin: 0.5rem 0;
 }
-
 .filter-option {
-  margin-right: 1rem;
-}
+	margin-right: 1rem;
 
-.filter-option label,
-.filter-option input {
-  vertical-align: middle;
-}
+	label,
+	input {
+		vertical-align: middle;
+	}
 
-.filter-option label {
-  margin-left: 0.25rem;
-}
+	label {
+		margin-left: 0.25rem;
+		text-transform: capitalize;
+	}
 
-.filter-option.active label {
-  font-weight: bold;
+	&.active label {
+		font-weight: bold;
+	}
 }
 </style>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -63,9 +63,15 @@ export default {
 			isLoading: false,
 			error: null,
 			activeFilters: {
-				frontend: true,
-				backend: true,
-				career: true,
+				frontend: {
+					isActive: true,
+				},
+				backend: {
+					isActive: true,
+				},
+				career: {
+					isActive: true,
+				},
 			},
 		}
 	},
@@ -79,13 +85,22 @@ export default {
 		filteredCoaches() {
 			const coaches = this.$store.getters['coaches/coaches']
 			return coaches.filter((coach) => {
-				if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+				if (
+					this.activeFilters.frontend.isActive &&
+					coach.areas.includes('frontend')
+				) {
 					return true
 				}
-				if (this.activeFilters.backend && coach.areas.includes('backend')) {
+				if (
+					this.activeFilters.backend.isActive &&
+					coach.areas.includes('backend')
+				) {
 					return true
 				}
-				if (this.activeFilters.career && coach.areas.includes('career')) {
+				if (
+					this.activeFilters.career.isActive &&
+					coach.areas.includes('career')
+				) {
 					return true
 				}
 				return false
