7c06d91bf491292672f74f08d9eeca5335c06875
negro-vinilo <52874360+negro-vinilo@users.noreply.github.com>
2024-01-24 14:06:58 +1100

    [vue] CoachFilter & CoachesList components
    CoachFilter
    - removing code duplication
    - changed data object properties and updated scoped style from css to scss
    
    CoachesList
    - updated component data structure to reflect CoachFilter modifications

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,3 +1,5 @@
+CoachesList
+
 <template>
 	<div>
 		<base-dialog
@@ -63,9 +65,15 @@ export default {
 			isLoading: false,
 			error: null,
 			activeFilters: {
-				frontend: true,
-				backend: true,
-				career: true,
+				frontend: {
+					isActive: true,
+				},
+				backend: {
+					isActive: true,
+				},
+				career: {
+					isActive: true,
+				},
 			},
 		}
 	},
@@ -79,13 +87,22 @@ export default {
 		filteredCoaches() {
 			const coaches = this.$store.getters['coaches/coaches']
 			return coaches.filter((coach) => {
-				if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+				if (
+					this.activeFilters.frontend.isActive &&
+					coach.areas.includes('frontend')
+				) {
 					return true
 				}
-				if (this.activeFilters.backend && coach.areas.includes('backend')) {
+				if (
+					this.activeFilters.backend.isActive &&
+					coach.areas.includes('backend')
+				) {
 					return true
 				}
-				if (this.activeFilters.career && coach.areas.includes('career')) {
+				if (
+					this.activeFilters.career.isActive &&
+					coach.areas.includes('career')
+				) {
 					return true
 				}
 				return false
