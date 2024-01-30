bb984fd9ede2dbc55fde065e2dd6320c1db6b23d
negro-vinilo <52874360+negro-vinilo@users.noreply.github.com>
2024-01-30 15:26:50 +1100

    [vue, scss] CoachForm and ContactCoachForm components
    - style scoped css refactored to scss on both components
    - missing conditional invalid class added to CoachForm paragraph DOM
    -  conditional invalid classes refactored to match CoachForm
    - clearValidity method's conditional check updated for email input

--- src/components/requests/ContactCoachForm.vue
+++ src/components/requests/ContactCoachForm.vue
@@ -1,18 +1,21 @@
 <template>
 	<form @submit.prevent="submitForm">
-		<div class="form-control" :class="{ invalid: !email.isValid }">
-			<label for="email">Your E-Mail</label>
+		<div :class="['form-control', { invalid: !email.isValid }]">
+			<label for="email">{{ email.label }}</label>
 			<input
-				type="email"
+				:type="email.type"
 				id="email"
 				v-model.trim="email.val"
 				@blur="clearValidity('email')"
 			/>
-			<p v-if="!email.isValid">Please enter a valid email address.</p>
+			<p :class="{ invalid: !email.isValid }" v-if="!email.isValid">
+				Please enter a valid email address.
+			</p>
 		</div>
-		<div class="form-control" :class="{ invalid: !message.isValid }">
-			<label for="message">Message</label>
+		<div :class="['form-control', { invalid: !message.isValid }]">
+			<label for="message">{{ message.label }}</label>
 			<textarea
+				:type="message.type"
 				rows="5"
 				id="message"
 				v-model.trim="message.val"
@@ -31,10 +34,14 @@ export default {
 	data() {
 		return {
 			email: {
+				label: 'Your E-Mail',
+				type: 'email',
 				val: '',
 				isValid: true,
 			},
 			message: {
+				label: 'Message',
+				type: 'textarea',
 				val: '',
 				isValid: true,
 			},
@@ -43,10 +50,15 @@ export default {
 	},
 	methods: {
 		clearValidity(input) {
-			this[input].isValid = true
+			if (this[input].val === '' || !this[input].val.includes('@')) {
+				this[input].isValid = false
+			} else {
+				this[input].isValid = true
+			}
 		},
 		validateForm() {
 			this.formIsValid = true
+
 			if (this.email.val === '') {
 				this.email.isValid = false
 				this.formIsValid = false
@@ -74,50 +86,53 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 form {
-	margin: 1rem;
-	border: 1px solid #ccc;
-	border-radius: 12px;
-	padding: 1rem;
-}
+	.form-control {
+		margin: 0.5rem 0;
 
-.form-control {
-	margin: 0.5rem 0;
-}
+		label {
+			font-weight: bold;
+			display: block;
+			margin-bottom: 0.5rem;
+		}
 
-label {
-	font-weight: bold;
-	margin-bottom: 0.5rem;
-	display: block;
-}
+		input,
+		textarea {
+			border: 1px solid $color-silver;
+			display: block;
+			font: inherit;
+			padding: 0.15rem;
+			width: 100%;
 
-input,
-textarea {
-	display: block;
-	width: 100%;
-	font: inherit;
-	border: 1px solid #ccc;
-	padding: 0.15rem;
-}
+			&:focus {
+				background-color: $color-selago;
+				border-color: $color-pigment-indigo;
+				outline: none;
+			}
+		}
 
-input:focus,
-textarea:focus {
-	border-color: #3d008d;
-	background-color: #faf6ff;
-	outline: none;
-}
+		textarea {
+			height: 150px;
+			resize: none;
+		}
 
-.actions {
-	text-align: center;
-}
+		&.invalid {
+			p {
+				color: $color-red;
+				margin-top: 0.25rem;
+			}
 
-.invalid label {
-	color: red;
-}
-
-.invalid input,
-.invalid textarea {
-	border: 1px solid red;
+			input,
+			textarea {
+				border: 1px solid $color-red;
+			}
+		}
+	}
+	.actions {
+		justify-content: center;
+		margin: 0 auto;
+		text-align: center;
+	}
 }
 </style>
