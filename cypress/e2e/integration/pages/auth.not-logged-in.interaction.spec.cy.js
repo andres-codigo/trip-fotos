05518b136fa39af2beb7c2814af3505cbb3b8e30
negro-codigo-cuenta <52874360+andres-codigo-cuenta@users.noreply.github.com>
2024-10-11 16:52:44 +1100

    [spec.cy.js] auth password cypress test
    - display and count down remaining password characters needed to meet minimum password length requirement in error message test added
    - error message string updated for error message consistency

--- cypress/e2e/integration/pages/auth.not-logged-in.interaction.spec.cy.js
+++ cypress/e2e/integration/pages/auth.not-logged-in.interaction.spec.cy.js
@@ -91,6 +91,45 @@ describe('Trip Fotos user auth page > not logged in', () => {
 		invalidInputTest('@userAuthPassword', passwordInputErrorMessage)
 	})
 
+	it('displays and counts down remaining password characters needed to meet minimum password length requirement in error message', () => {
+		const inputSelector = '@userAuthPassword'
+		const errorMessageSelector = 'p'
+		const minLength = 8
+		const password = '2345678'
+
+		cy.get(inputSelector)
+			.find('input')
+			.focus()
+			.should('have.value', '')
+			.blur()
+
+		// Initial error message indicating required and remaining characters
+		cy.get(errorMessageSelector).should(
+			'contain.text',
+			`Your password must be minimum ${minLength} characters long! ${minLength} characters left.`
+		)
+
+		// Type characters to the input
+		for (let i = 0; i < password.length; i++) {
+			cy.get(inputSelector).find('input').type(password[i])
+
+			// Calculate remaining characters needed
+			const currentLength = i + 1 // Current input length
+			const remainingChars = minLength - currentLength
+
+			// Update the error message check based on remaining characters
+			if (remainingChars > 0 && remainingChars < minLength) {
+				cy.get(errorMessageSelector).should(
+					'contain.text',
+					`Your password must be minimum ${minLength} characters long! ${remainingChars} characters left.`
+				)
+			} else {
+				// Check error message removed once minimum password character length is met
+				cy.get(errorMessageSelector).should('not.exist')
+			}
+		}
+	})
+
 	it('displays email and password error messages when inputs are empty and login button clicked', () => {
 		cy.get('@userAuthenticationContainer')
 			.get('@userAuthLoginButton')
