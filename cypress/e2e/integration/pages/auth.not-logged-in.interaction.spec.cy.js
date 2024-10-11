71b02da38679327e30143dd70091527195c6493f
negro-codigo-cuenta <52874360+andres-codigo-cuenta@users.noreply.github.com>
2024-10-11 15:03:51 +1100

    [spec.cy.js] Auth Cypress Tests
    - cypress test refactor

--- cypress/e2e/integration/pages/auth.not-logged-in.interaction.spec.cy.js
+++ cypress/e2e/integration/pages/auth.not-logged-in.interaction.spec.cy.js
@@ -1,12 +1,12 @@
 describe('Trip Fotos user auth page > not logged in', () => {
-	let emptyEmailInputErrorMessage
-	let emptyPasswordInputErrorMessage
-	let emptyEmailTest
-	let emptyPasswordTest
+	let emailInputErrorMessage
+	let passwordInputErrorMessage
+	let invalidInputTest
 
 	beforeEach(() => {
 		cy.visit(Cypress.env('auth_url'))
 
+		// Aliases
 		cy.get('[data-cy="user-authentication"]').as(
 			'userAuthenticationContainer'
 		)
@@ -14,70 +14,89 @@ describe('Trip Fotos user auth page > not logged in', () => {
 		cy.get('[data-cy="user-auth-password"]').as('userAuthPassword')
 		cy.get('[data-cy="user-auth-login-button"]').as('userAuthLoginButton')
 
-		emptyEmailInputErrorMessage = 'Please enter a valid email address.'
-		emptyPasswordInputErrorMessage =
+		// Error Messages
+		emailInputErrorMessage = 'Please enter a valid email address.'
+		passwordInputErrorMessage =
 			'Your password must be minimum 8 characters long! 8 characters left'
 
-		emptyEmailTest = () => {
-			// Check if the input field is empty
-			cy.get('@userAuthEmail').find('input').should('have.value', '') // Verify input is empty
-
+		// Functions
+		invalidInputTest = (alias, errorMessage) => {
 			// Check the classes for validation
-			cy.get('@userAuthEmail')
+			cy.get(alias)
 				.should('have.class', 'form-control')
 				.and('have.class', 'invalid')
 
 			// Verify the error message is displayed
-			cy.get('@userAuthEmail')
-				.find('p')
-				.should('contain.text', emptyEmailInputErrorMessage)
-		}
-
-		emptyPasswordTest = () => {
-			// Check if the input field is empty
-			cy.get('@userAuthPassword').find('input').should('have.value', '') // Verify input is empty
-
-			// Check the classes for validation
-			cy.get('@userAuthPassword')
-				.should('have.class', 'form-control')
-				.and('have.class', 'invalid')
-
-			// Verify the error message is displayed
-			cy.get('@userAuthPassword')
-				.find('p')
-				.should('contain.text', emptyPasswordInputErrorMessage)
+			cy.get(alias).find('p').should('contain.text', errorMessage)
 		}
 	})
 
-	it('displays email error message when input field is empty on focus and then blur ', () => {
+	it('displays email error message when input is empty', () => {
 		cy.get('@userAuthenticationContainer')
 			.get('@userAuthEmail')
 			.find('input')
 			.focus()
+			.should('have.value', '')
 			.blur()
-			.then(() => {
-				emptyEmailTest()
-			})
+
+		invalidInputTest('@userAuthEmail', emailInputErrorMessage)
 	})
 
-	it('displays password error message when input field is empty on focus and then blur ', () => {
+	it('displays email error message when email is invalid', () => {
+		cy.get('@userAuthenticationContainer')
+			// Enter invalid email address
+			.get('@userAuthEmail')
+			.find('input')
+			.focus()
+			.type('invalid@email')
+			.should('have.value', 'invalid@email')
+
+		invalidInputTest('@userAuthEmail', emailInputErrorMessage)
+	})
+	it('displays no email error message when email typed becomes valid', () => {
+		const firstCharacterOfValidEmail = 'v'
+		const remainderOfValidEmail = 'alid@email.com'
+
+		cy.get('@userAuthenticationContainer')
+			// Enter first character of email address
+			.get('@userAuthEmail')
+			.find('input')
+			.focus()
+			.type(firstCharacterOfValidEmail)
+			.should('have.value', firstCharacterOfValidEmail)
+
+		// Loop through each character and type it
+		for (let char of remainderOfValidEmail) {
+			cy.get('@userAuthEmail').find('input').type(char)
+		}
+
+		// Check CSS classes does not include 'invalid' class
+		cy.get('@userAuthEmail')
+			.should('have.class', 'form-control')
+			.and('not.have.class', 'invalid')
+
+			// Verify the error message element is removed from the DOM
+			.find('p')
+			.should('not.exist')
+	})
+
+	it('displays password error message when input is empty', () => {
 		cy.get('@userAuthenticationContainer')
 			.get('@userAuthPassword')
 			.find('input')
 			.focus()
+			.should('have.value', '') // Verify input is empty
 			.blur()
-			.then(() => {
-				emptyPasswordTest()
-			})
+
+		invalidInputTest('@userAuthPassword', passwordInputErrorMessage)
 	})
 
-	it('displays email and password error messages when input fields are empty and login button clicked ', () => {
+	it('displays email and password error messages when inputs are empty and login button clicked', () => {
 		cy.get('@userAuthenticationContainer')
 			.get('@userAuthLoginButton')
 			.click()
-			.then(() => {
-				emptyEmailTest()
-				emptyPasswordTest()
-			})
+
+		invalidInputTest('@userAuthEmail', emailInputErrorMessage)
+		invalidInputTest('@userAuthPassword', passwordInputErrorMessage)
 	})
 })
