8e6cf1bc49531dce5cf37fa0b32fe853c6b12f51
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2024-10-18 15:26:52 +1100

    [JA, README] api environment key/vallues updated to remove duplicates

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,12 +1,12 @@
 export const APIConstants = Object.freeze({
 	// API
-	API_URL: import.meta.env.VITE_API_URL,
-	API_KEY: import.meta.env.VITE_API_KEY,
+	API_URL: import.meta.env.VITE_FIREBASE_API_URL,
+	API_KEY: import.meta.env.VITE_FIREBASE_API_KEY,
 	API_AUTH_LOGIN_MODE: 'login',
 	API_AUTH_SIGNUP_MODE: 'signup',
 
 	// DATABASE
-	BASE_URL: import.meta.env.VITE_BACKEND_BASE_URL,
+	BASE_URL: import.meta.env.VITE_FIREBASE_BACKEND_BASE_URL,
 	GET: 'GET',
 	POST: 'POST',
 	PUT: 'PUT',
